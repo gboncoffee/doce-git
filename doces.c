428ce761ba006516d045252c7406e8f5cbe9e307
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2024-07-27 17:03:10 -0300

    Adicionada mensagem saúdando doces Uwu!

--- doces.c
+++ doces.c
@@ -11,5 +11,7 @@ int main(void) {
   for (int i = 0; i < DOCES; i++)
     printf("Meu nome é %s e eu gosto de %s.\n", doces[i][0], doces[i][1]);
 
+  printf("Doces são muito gostosos e fofos!\n");
+
   return 0;
 }
